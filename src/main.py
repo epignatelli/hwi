72d56acdc286672658a1337e0052e76dd271983e
Eduardo <eduardo.pignatelli@burohappold.com>
2018-12-09 00:17:28 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,4 +2,6 @@ import create_model, preprocess, predict, train, postprocess, display_data
 
 # if not an imported module
 if __name__ == "__main__":
-    create_model.build_model(weights="../weights/nasnet-imagenet_weights.h5")
+    model = create_model.build_model(weights="../weights/nasnet-imagenet_weights.h5")
+    trained = train.train(model, "../data/")
+
